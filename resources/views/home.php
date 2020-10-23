df7f1c116c57d74bd0d751cb579c7b0c0a947b86
GABRIEL B <gabgamardo25@gmail.com>
2020-10-23 16:03:25 -0500

    Primer commit

---
+++ resources/views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Favicon -->
+    <!-- estilos -->
+</head>
+<body>
+    <!-- header -->
+    <!-- nav -->
+    <h1>Bienvenido a la pagina principal</h1>
+
+    <!-- footer -->
+
+    <!-- script -->
+</body>
+</html>
